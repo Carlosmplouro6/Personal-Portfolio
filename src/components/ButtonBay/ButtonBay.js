8be87e7cc3d9ffb3858e5a8e1c702020151b5c2e
Carlos Louro <carlosmplouro6@gmail.com>
2020-07-13 16:01:25 +0100

    added functionalities and links

--- src/components/ButtonBay/ButtonBay.js
+++ src/components/ButtonBay/ButtonBay.js
@@ -1,6 +1,11 @@
 import React, { useContext } from "react";
 import "./ButtonBay.css";
 
+import folderIcon from "../../imgs/FolderIcon.svg";
+import txtIcon from "../../imgs/TxtIcon.svg";
+import githubIcon from "../../imgs/GithubIcon.svg";
+import linkedinIcon from "../../imgs/LinkedinIcon.svg";
+
 import { PortfolioContext } from "../../context/PortfolioContext";
 
 const ButtonBay = () => {
@@ -99,29 +104,7 @@ const ButtonBay = () => {
           ])
         }
       >
-        <svg
-          className="Folder"
-          fill="#000000"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 32 32"
-          width="50px"
-          height="50px"
-        >
-          <path
-            fill=" #e6cb73"
-            stroke="black"
-            strokeMiterlimit="10"
-            strokeWidth="1"
-            d="M26 4L6 4 6 27 18 29.5 18 27 24 27 24 15 26 13z"
-          />
-          <path
-            fill="none"
-            stroke="black"
-            strokeMiterlimit="10"
-            strokeWidth="1"
-            d="M18 27L18 17.5 16 15.5 16 6.5 6 4"
-          />
-        </svg>
+        <img src={folderIcon} alt="" />
         <p className="ButtonName">Projects</p>
       </div>
 
@@ -130,27 +113,39 @@ const ButtonBay = () => {
         onClick={() =>
           SetTxt([
             {
-              name: "projects",
-              txt: "this is a test TXT",
+              name: "About Me",
+              txt:
+                "Hello, welcome to my portfolio my name is Carlos Louro im portuguese and i have a degree in biomedical technologies, i love programming and i am used to creating fullstack websites and games. ",
             },
           ])
         }
       >
-        <svg
-          width="50px"
-          height="50px"
-          enableBackground="new 0 0 128 170"
-          version="1.1"
-          viewBox="0 0 128 170"
-        >
-          <path
-            d="M127.199,29l0.211-0.004L99,0.379V0h-0.793L98.09-0.117L97.973,0H9.586C4.063,0,0,4.477,0,10v150  c0,5.523,4.063,10,9.586,10h108c5.522,0,10.414-4.477,10.414-10V29H127.199z M109,121H98v29h-8v-29H80v-7.026l-11.133,17.202  L81.136,150h-9.88l-7.748-11.96L55.812,150h-9.776l12.131-18.616L47,114.14V121H37v29h-8v-29H18v-7h28.867h0.313h9.411l6.916,10.764  L70.424,114h9.567h0.208H109V121z M108.586,29C103.063,29,99,24.523,99,19V4.622L122.965,29H108.586z"
-            fill="#ffffff"
-          />
-        </svg>
+        <img src={txtIcon} alt="" />
 
-        <p className="ButtonName">Projects</p>
+        <p className="ButtonName">AboutMe</p>
       </div>
+
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        className="Button"
+        href="https://github.com/Carlosmplouro6"
+      >
+        <img className="" src={githubIcon} alt="" />
+
+        <p className="ButtonName">Github</p>
+      </a>
+
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        className="Button"
+        href="https://github.com/Carlosmplouro6"
+      >
+        <img className="" src={linkedinIcon} alt="" />
+
+        <p className="ButtonName">LinkedIn</p>
+      </a>
     </div>
   );
 };
--- src/components/Screen/Screen.js
+++ src/components/Screen/Screen.js
@@ -8,6 +8,66 @@ import { PortfolioContext } from "../../context/PortfolioContext";
 const Screen = () => {
   const { openFolder, openTxt } = useContext(PortfolioContext);
 
+  var TxtType = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = "";
+    this.tick();
+    this.isDeleting = false;
+  };
+
+  TxtType.prototype.tick = function () {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) {
+      delta /= 2;
+    }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+
+    setTimeout(function () {
+      that.tick();
+    }, delta);
+  };
+
+  window.onload = function () {
+    var elements = document.getElementsByClassName("typewrite");
+    for (var i = 0; i < elements.length; i++) {
+      var toRotate = elements[i].getAttribute("data-type");
+      var period = elements[i].getAttribute("data-period");
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML =
+      ".typewrite > .wrap { border-right: 0.08em solid transparent}";
+    document.body.appendChild(css);
+  };
+
   return (
     <div className="Screen">
       {openFolder ? (
@@ -22,7 +82,14 @@ const Screen = () => {
       ) : null}
       <div className="NameDiv">
         <h1 className="DevName">Carlos Louro</h1>
-        <h3 className="OcupationTitle">Web Developer</h3>
+        <p
+          href=""
+          class="typewrite"
+          data-period="2000"
+          data-type='[ "Web Developer", "FullStack Developer", "FrontEnd Developer", "BackEnd Developer" ]'
+        >
+          <span class="wrap"></span>
+        </p>
       </div>
       <div className="ButtonBay">
         <ButtonBay />
