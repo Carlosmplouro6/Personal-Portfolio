12a36180392a246fe4e9ec7230a4e30eb86e7f69
Carlos Louro <carlosmplouro6@gmail.com>
2020-07-24 20:27:39 +0100

    Updates

---
+++ precache-manifest.dba9a7d48777aa965bce134e3c9281d0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15a6cf9ad4cec48c736de2eb0adce9c4",
+    "url": "/Personal-Portfolio/index.html"
+  },
+  {
+    "revision": "c016d68c135d76384fe1",
+    "url": "/Personal-Portfolio/static/css/main.0f009358.chunk.css"
+  },
+  {
+    "revision": "c3885f63c66fa2179c23",
+    "url": "/Personal-Portfolio/static/js/2.6841ab25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Personal-Portfolio/static/js/2.6841ab25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c016d68c135d76384fe1",
+    "url": "/Personal-Portfolio/static/js/main.eb6db7cc.chunk.js"
+  },
+  {
+    "revision": "3efdb0a7bdb498e85433",
+    "url": "/Personal-Portfolio/static/js/runtime-main.4236d556.js"
+  },
+  {
+    "revision": "3de414a1ef34ebca0f7c981f23efd0f6",
+    "url": "/Personal-Portfolio/static/media/Consequat.3de414a1.png"
+  },
+  {
+    "revision": "8f70a52a0197a2b14516462740b56796",
+    "url": "/Personal-Portfolio/static/media/CoronaFocus.8f70a52a.png"
+  },
+  {
+    "revision": "d121cf7af8a74c79f60bddcbab9aa26f",
+    "url": "/Personal-Portfolio/static/media/FolderIcon.d121cf7a.svg"
+  },
+  {
+    "revision": "f23f3db2b21e60531d2a018076beffb2",
+    "url": "/Personal-Portfolio/static/media/GithubIcon.f23f3db2.svg"
+  },
+  {
+    "revision": "113511bdb0c6185d6b4b27c53af304df",
+    "url": "/Personal-Portfolio/static/media/LinkedinIcon.113511bd.svg"
+  },
+  {
+    "revision": "8feb412f805c860d647d13698b400b3e",
+    "url": "/Personal-Portfolio/static/media/MonitorPortfolioImproved.8feb412f.png"
+  },
+  {
+    "revision": "8f40660c60702c346669727fc3cf210a",
+    "url": "/Personal-Portfolio/static/media/TxtIcon.8f40660c.svg"
+  },
+  {
+    "revision": "fb585b62b6dac3697bd891f9795b9c5d",
+    "url": "/Personal-Portfolio/static/media/imageIcon.fb585b62.svg"
+  },
+  {
+    "revision": "a2c5e63c69c805577db502e46b4faffe",
+    "url": "/Personal-Portfolio/static/media/leftArrow.a2c5e63c.svg"
+  },
+  {
+    "revision": "33513ff46a603b84af09f618c94a5079",
+    "url": "/Personal-Portfolio/static/media/linkIcon.33513ff4.svg"
+  },
+  {
+    "revision": "c4b43d1fef93c506090c3c9f9a6d9f21",
+    "url": "/Personal-Portfolio/static/media/segoe-ui.c4b43d1f.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4fbbde5ece77ef12e15e9d24e57c617f.js"
+  "/Personal-Portfolio/precache-manifest.dba9a7d48777aa965bce134e3c9281d0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Personal-Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
